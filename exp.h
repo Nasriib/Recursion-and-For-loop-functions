73bb60b7fe88a3accf8edb1a7b8ccbd76bf603b1
Nasri Ibrahim <99497859+Nasriib@users.noreply.github.com>
2022-04-16 13:29:39 -0400

    Create exp.h

---
+++ exp.h
@@ -0,0 +1,12 @@
+#ifndef ISIP_EXAMPLE
+#define ISIP_EXAMPLE
+//include files
+//
+#include <stdio.h>
+#include <stdlib.h>
+//included functions
+//
+long mypow_for(long i, long j);
+long mypow_rec(long i, long j);
+
+#endif
